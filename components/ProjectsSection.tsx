bb719d05cc2bc241e276660ae490459657517e75
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-05-12 10:51:30 +0600

    miller

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -9,7 +9,7 @@ const projects = [
     name: "Miller",
     description:
       "Miller is online store, where you can buy coffee and various coffee drinks according to your taste and color!",
-    image: "/Miller.png",
+    image: "/miller.png",
     github: "https://github.com/jodar-Nimatov/Miller",
     link: "https://miller-shop.netlify.app/",
   },
